f64ea4acc37980a760f472883270be515a37332d
dexxhawk <Dsshnyakin@yandex.ru>
2024-11-16 13:57:59 +0300

    Add description to readme.md

--- mod/packages/b.py
+++ mod/packages/b.py
@@ -1,8 +1,8 @@
-# Так не работает:
+# Так не работает, protoc генерирует именно такие импорты:
 import a as lib 
 
 # Так работает:
-# import packages.a as lib 
+# import packages.a as lib
 
 def bar():
     print(lib.foo())
